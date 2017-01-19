18ef44feb0d7ba20bad597aa19fe701ee7f0ec28
cesar <cesar@magic3w.com>
2017-01-19 12:35:22 +0100

    Added the wallpapers so asphagnum can check them out

--- bin/templates/home/index.php
+++ bin/templates/home/index.php
@@ -1,2 +1,21 @@
 
-<h1>Ping</h1>
+<div class="home-billboard">
+	<div class="row1">
+		<div class="span1">
+			<div class="spacer" style="height: 100px;"></div>
+			<h1>Ping</h1>
+			<div class="spacer" style="height:  10px;"></div>
+			<p>Notification feeds for your app</p>
+		</div>
+	</div>
+</div>
+
+
+<script type="text/javascript">
+(function () {
+	var html = document.body;
+	html.style.background = 'url(<?= URL::asset('img/wallpaper.jpg') ?>)';
+	html.style.backgroundSize     = 'cover';
+	html.style.backgroundPosition = 'center';
+}());
+</script>
