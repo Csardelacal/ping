638e5cc9044d573c630b795eb6212feaca2343e4
cesar <cesar@magic3w.com>
2019-05-02 14:00:59 +0200

    Documented the home controller and removed a few syntax errors from the feed controller.

--- bin/controllers/home.php
+++ bin/controllers/home.php
@@ -1,63 +1,23 @@
 <?php
 
 /**
- * Prebuilt test controller. Use this to test all the components built into
- * for right operation. This should be deleted whe using Spitfire.
+ * The home controller doesn't do much really. It just displays the welcome page
+ * if the user is not logged in and redirects to the feed if the user is logged
+ * in.
+ * 
+ * @author César de la Cal Bretschneider <cesar@magic3w.com>
  */
-
 class HomeController extends AppController
 {
+	
+	/**
+	 * Home just needs to redirect to the feed itself or display a splash screen
+	 * if the user is not yet logged in.
+	 */
 	public function index() {
 		if ($this->user) {
 			$this->response->setBody('Redirecting...')->getHeaders()->redirect(url('feed'));
 		}
 	}
 	
-	public function test() {
-		$r = request('http://localhost/cloudy/pool1/bucket/read/i353k45j.json');
-		$r->get('signature', (string)$this->sso->makeSignature('1488571465'));
-		
-		$response = $r->send()->json();
-		$server   = $response->payload->master->hostname;
-		
-		$r = request($server . '/media/create.json');
-		$r->get('signature', (string)$this->sso->makeSignature('1488571465'));
-		$r->post('bucket', 'i353k45j');
-		$r->post('name', base_convert(time(), 10, 32) . '.jpg');
-		$r->post('file', new CURLFile('/home/cesar/Pictures/1472761257.crow3000_img_0043.jpg'));
-		$r->post('mime', mime('/home/cesar/Pictures/1472761257.crow3000_img_0043.jpg'));
-		
-		$response = $r->send()->json();
-		$mediauuid = $response->uniqid;
-		
-		var_dump($response);
-		sleep(1);
-		
-		$r = request($server . '/media/read/' . $mediauuid . '.json');
-		$r->get('signature', (string)$this->sso->makeSignature('1488571465'));
-		$response = $r->send()->json();
-		
-		
-		foreach ($response->servers as $server) {
-			echo sprintf('<img src="%s/file/retrieve/link/%s" width="100" height="100">', $server->hostname, reset($response->links)->uniqid);
-		}
-		
-		die(sprintf('<a href="%s">Delete</a>', url('home', 'testDelete', $mediauuid)));
-	}
-	
-	public function testDelete($uniqid) {
-		
-		$r = request('http://localhost/cloudy/pool1/bucket/read/i353k45j.json');
-		$r->get('signature', (string)$this->sso->makeSignature('1488571465'));
-		
-		$response = $r->send()->json();
-		$server   = $response->payload->master->hostname;
-		
-		
-		$r = request($server . '/media/delete/' . $uniqid . '.json');
-		$r->get('signature', (string)$this->sso->makeSignature('1488571465'));
-		$r->send();
-		
-		die();
-	}
 }
