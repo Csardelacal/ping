baf910bbec6d3442f41fb6c810f6105f1a625c5e
cesar <cesar@magic3w.com>
2019-10-10 08:20:13 +0200

    Fix for T36
    Summary:
    NOTE: Requires D120 to work
    Fixes T36
    
    Test Plan: Pings should now be creatable from ping and externals
    
    Reviewers: sebastian
    
    Reviewed By: sebastian
    
    Maniphest Tasks: T36
    
    Differential Revision: https://phabricator.magic3w.com/D121

--- bin/templates/media/upload.json.php
+++ bin/templates/media/upload.json.php
@@ -1,5 +1,9 @@
 <?php
 
+
+current_context()->response->getHeaders()->set('Access-Control-Allow-Origin', '*');
+current_context()->response->getHeaders()->set('Access-Control-Allow-Headers', 'Content-Type');
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
