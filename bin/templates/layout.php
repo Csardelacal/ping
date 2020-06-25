5288b9a789d7a951be4bd2ae32921c8978da41a2
cesar <cesar@magic3w.com>
2020-06-25 11:43:48 +0200

    Fixes activity
    Test Plan: Activity fix - guests receive proper notifications now, they are not directed to registered user by mistake
    
    Reviewers: sebastian
    
    Reviewed By: sebastian
    
    Differential Revision: https://phabricator.magic3w.com/D310

--- bin/templates/layout.php
+++ bin/templates/layout.php
@@ -9,19 +9,6 @@
 		<meta name="_scss" content="<?= \spitfire\SpitFire::baseUrl() ?>/assets/scss/_/js/">
 		<meta name="ping.endpoint" content="<?= rtrim(\spitfire\SpitFire::baseUrl(), '/') ?>/">
 		
-		<?php if (\spitfire\core\Environment::get('analytics.id')): ?>
-		<script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-			ga('create', '<?= \spitfire\core\Environment::get('analytics.id') ?>', 'auto');
-			ga('send', 'pageview');
-
-		 </script>
-		 <?php endif; ?>
-		
 		<script src="<?= spitfire\core\http\URL::asset('js/m3/depend.js') ?>" type="text/javascript"></script>
 		<script src="<?= spitfire\core\http\URL::asset('js/m3/depend/router.js') ?>" type="text/javascript"></script>
 		
