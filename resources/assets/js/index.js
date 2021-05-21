0c993b2594616deb518e1ae59415a79254be8bd5
cesar <cesar@magic3w.com>
2021-05-21 11:51:57 +0200

    Removed depend from layout
    Test Plan: The javascript in layout should no longer depend on depend()
    
    Reviewers: cesar
    
    Reviewed By: cesar
    
    Differential Revision: https://phabricator.magic3w.com/D789

--- resources/assets/js/index.js
+++
@@ -1,4 +0,0 @@
-import lysine from "lysine/dist/lysine.js"
-
-console.log('Welcome to ping');
-console.log(typeof lysine.view)
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,6 @@ let mix = require("laravel-mix");
 
 mix
 	.js('resources/assets/js/feed/index.js', 'public/js/feed/index.js')
-	.js('resources/assets/js/index.js', 'public/js/index.js')
+	.js('resources/assets/js/app.js', 'public/js/app.js')
 	.sass('resources/assets/css/app.scss', 'css/app.css')
 	.setPublicPath('./public');
