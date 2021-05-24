fe2f201465e8c233be91b2b59e0eab518b3dd201
cesar <cesar@magic3w.com>
2021-05-24 13:03:28 +0200

    Removed depend() from user/show
    Test Plan: User/show does no longer make use of depend
    
    Reviewers: cesar
    
    Reviewed By: cesar
    
    Differential Revision: https://phabricator.magic3w.com/D793

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,6 +5,6 @@ mix
 	.js('resources/assets/js/app.js', 'public/js/app.js')
 	.js('resources/assets/js/activity/index.js', 'public/js/activity/index.js')
 	.js('resources/assets/js/ping/detail.js', 'public/assets/js/ping/detail.js')
-	.js('resources/assets/js/index.js', 'public/js/index.js')
+	.js('resources/assets/js/user/show.js', 'public/js/user/show.js')
 	.sass('resources/assets/css/app.scss', 'css/app.css')
 	.setPublicPath('./public');
