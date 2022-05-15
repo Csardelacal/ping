0febbb6a4da5422459037dbfafc6a88d0fa885b9
Andreas Mayerhofer <gitea@cheashugames.de>
2022-05-15 19:32:48 +0200

    Replace Media Error Exception with a less site breaking alternative

--- bin/models/media/missingThumb.php
+++ bin/models/media/missingThumb.php
@@ -17,7 +17,7 @@ class MissingThumbModel extends ThumbModel
 		// TODO: temporary styling until overhaul with vue and tailwind
 		$style = 'font-weight: bold; font-style: italic; text-align: center;';
 		$style.= 'border: 1px solid black; padding: 5px; font-size: 0.9em; margin: 5px';
-		return '<div style="${style}">Media Error</div>';
+		return '<div style="' . $style . '">Media Error</div>';
 	}
 	
 	public function getMediaEmbed()
